35a0ffa8f76a8a5fe9a5c27825cebfe73c0b35fa
Russ Weas <russweas@gmail.com>
2021-08-31 14:45:36 -0500

    Fixed bug with Digital Ocean auto deployment
    changed api port to 80
    
    using non-release compilations for testing
    
    fixed bug in Dockerfile
    
    updated dockerfile
    
    fix port
    
    bind to 0.0.0.0 instead of localhost

--- api/src/main.rs
+++ api/src/main.rs
@@ -3,9 +3,9 @@ use std::net::TcpListener;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let address = format!("{}:{}", "localhost", 8000);
+    let address = format!("{}:{}", "0.0.0.0", 8000);
     let listener = TcpListener::bind(address.clone())?;
-
+    println!("Starting server");
     run(listener)?.await?;
     Ok(())
 }
