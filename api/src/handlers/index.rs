70c4db4a33118e800e32c3ce1c38292e476177a3
Russ Weas <russweas@gmail.com>
2021-09-02 17:32:14 -0500

    Create User struct (#10)

--- api/src/handlers/index.rs
+++ api/src/handlers/index.rs
@@ -1,5 +1,22 @@
 use actix_web::HttpResponse;
+use chrono::NaiveDateTime;
+use serde::Serialize;
 use sqlx::postgres::{PgConnectOptions, PgPoolOptions};
+
+use uuid::Uuid;
+
+#[derive(sqlx::FromRow, Serialize, Debug)]
+pub struct User {
+    pub id: Uuid,
+    pub username: Option<String>,
+    pub email: String,
+    #[serde(skip_serializing)]
+    pub password_hash: String,
+    pub full_name: Option<String>,
+    #[serde(skip_serializing)]
+    pub created_at: NaiveDateTime,
+    pub updated_at: NaiveDateTime,
+}
 
 pub async fn hello() -> HttpResponse {
     let settings = PgConnectOptions::new()
@@ -20,21 +37,27 @@ pub async fn hello() -> HttpResponse {
         INSERT INTO users (username, email, password_hash, full_name)
         VALUES ($1, $2, $3, $4);
         "#,
-        "russweas",
-        "russweas@gmail.com",
+        "russweas22",
+        "russweas2@gmail.com",
         "109234r1uijfasdcn",
         "Russell Weas"
     )
     .execute(&connection_pool)
     .await;
     println!("{:?}", insert);
-    let users = sqlx::query!(
+    let users = sqlx::query_as::<_, User>(
         r#"
-        SELECT (username, email, password_hash, full_name)
+        SELECT *
         FROM users;
         "#,
     )
-    .fetch_one(&connection_pool)
-    .await;
-    HttpResponse::Ok().body(&format!("{:?}", users))
+    .fetch_all(&connection_pool)
+    .await
+    .expect("Failed to execute query on database");
+    println!("There are {} users in the database", users.len());
+
+    for entry in users {
+        println!("{:?}", entry);
+    }
+    HttpResponse::Ok().finish()
 }
