5a25bb9425055bd806ee7487acf004909dc96303
Russ Weas <russweas@gmail.com>
2021-09-26 15:24:24 -0500

    Bootstrap Digital Ocean deployment with no db connection timeout

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,13 @@ use hermod::{configuration::get_configuration, startup::Application};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let subscriber = get_subscriber("hermod".into(), "info".into(), std::io::stdout);
-    init_subscriber(subscriber);
+    // let subscriber = get_subscriber("hermod".into(), "info".into(), std::io::stdout);
+    // init_subscriber(subscriber);
 
-    let configuration = get_configuration().expect("Failed to read configuration.");
-    let application = Application::build(configuration).await?;
-    application.run_until_stopped().await?;
+    // let configuration = get_configuration().expect("Failed to read configuration.");
+    // let application = Application::build(configuration).await?;
+    // application.run_until_stopped().await?;
+    // loop {}
+    println!("The app deploys");
     Ok(())
 }
